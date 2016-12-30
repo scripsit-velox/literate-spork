20ece43b5bbcdaf47adc359ce2801dafb3e89639
Cameron <ck88.smith@gmail.com>
2016-12-30 13:21:19 -0600

    Using repetitive jQuery.

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 $(document).ready(function() {
     $('.selall-nil').click(function() {
-        $('.nil').prop('checked', $(this).prop('checked'));
+        $('.nil-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-k').click(function() {
+        $('.k-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-s').click(function() {
+        $('.s-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-t').click(function() {
+        $('.t-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-n').click(function() {
+        $('.n-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-h').click(function() {
+        $('.h-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-m').click(function() {
+        $('.m-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-y').click(function() {
+        $('.y-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-r').click(function() {
+        $('.r-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-w').click(function() {
+        $('.w-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-g').click(function() {
+        $('.g-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-z').click(function() {
+        $('.z-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-d').click(function() {
+        $('.d-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-b').click(function() {
+        $('.b-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-p').click(function() {
+        $('.p-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selall-v').click(function() {
+        $('.v-box').prop('checked', $(this).prop('checked'));
+        });
+    $('.selevery').click(function() {
+        $('.kana-box').prop('checked', $(this).prop('checked'));
         });
 });
